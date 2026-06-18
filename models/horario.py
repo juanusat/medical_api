3518b2afbb556cba325d05518c4ac728dafba6cf
LUIS MIGUEL BARBOZA VILCHEZ <pajaron202018@gmail.com>
2026-06-18 16:15:11 -0500

    Actualizacion de consulta horario_disponible

--- models/horario.py
+++ models/horario.py
@@ -5,21 +5,33 @@ class Horario:
         con = Conexion().open
         cursor = con.cursor()
 
+        # 1. Condiciones fijas que SIEMPRE se van a cumplir
         condiciones = [
             "hd.especialidad_id = %s",
-            "hd.fecha BETWEEN %s AND %s",
             "ehd.nombre = 'DISPONIBLE'",
             "em.nombre = 'ACTIVO'",
-            "CAST(CONCAT(hd.fecha, ' ', hd.hora_inicio) AS DATETIME) > NOW()",
+            "CAST(CONCAT(hd.fecha, ' ', hd.hora_inicio) AS DATETIME) > NOW()"
         ]
-        parametros = [especialidad_id, fecha_inicio, fecha_fin]
+        
+        # El primer parámetro siempre será el ID de la especialidad
+        parametros = [especialidad_id]
 
+        # 2. Condiciones DINÁMICAS (Solo si el cliente envía fechas)
+        if fecha_inicio and fecha_fin:
+            condiciones.append("hd.fecha BETWEEN %s AND %s")
+            parametros.append(fecha_inicio)
+            parametros.append(fecha_fin)
+        elif fecha_inicio: # Por si solo mandan una fecha fija
+            condiciones.append("hd.fecha = %s")
+            parametros.append(fecha_inicio)
+
+        # 3. Armamos el SQL con la solución a prueba de balas para los tipos de dato (CAST)
         sql = f"""
             SELECT
                 hd.id AS horario_disponible_id,
-                DATE_FORMAT(hd.fecha, '%%Y-%%m-%%d') AS fecha,
-                TIME_FORMAT(hd.hora_inicio, '%%H:%%i') AS hora_inicio,
-                TIME_FORMAT(hd.hora_fin, '%%H:%%i') AS hora_fin,
+                CAST(hd.fecha AS CHAR) AS fecha,
+                SUBSTRING(CAST(hd.hora_inicio AS CHAR), 1, 5) AS hora_inicio,
+                SUBSTRING(CAST(hd.hora_fin AS CHAR), 1, 5) AS hora_fin,
 
                 m.id AS medico_id,
                 CONCAT(m.nombres, ' ', m.apellidos) AS medico,
@@ -31,11 +43,11 @@ class Horario:
 
                 ehd.nombre AS estado_horario
             FROM horario_disponible hd
-            INNER JOIN medico m
+            INNER JOIN medico m 
                 ON hd.medico_id = m.id
-            INNER JOIN especialidad e
+            INNER JOIN especialidad e 
                 ON hd.especialidad_id = e.id
-            INNER JOIN estado_horario_disponible ehd
+            INNER JOIN estado_horario_disponible ehd 
                 ON hd.estado_horario_disponible_id = ehd.id
             INNER JOIN estado_medico em
                 ON m.estado_medico_id = em.id
@@ -45,6 +57,10 @@ class Horario:
                 hd.hora_inicio,
                 medico
         """
+        
+        print('SQL EJECUTADO:', sql)
+        print('PARAMETROS:', parametros)
+        
         cursor.execute(sql, parametros)
         resultados = cursor.fetchall()
         cursor.close()
